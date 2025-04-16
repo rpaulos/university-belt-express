8a220f91a1ab2a1f9f387d834fb3778d2b2caece
Rae <r.paulos.dev@gmail.com>
2025-04-16 21:57:15 +0800

    boiler plate

--- src/DatabaseHandler.java
+++ src/DatabaseHandler.java
@@ -2,5 +2,58 @@ import java.sql.*;
 import javax.naming.spi.DirStateFactory;
 
 public class DatabaseHandler {
-    
+    private static DatabaseHandler handler = null;
+    private static Statement stmt = null;
+    private static PreparedStatement pstatement = null;
+
+    public static DatabaseHandler getInstance() {
+        if (handler == null) {
+            handler = new DatabaseHandler();
+        }
+        return handler;
+    }
+
+    public static Connection getDBConnection()
+    {
+        Connection connection = null;
+        String dburl = "jdbc:mysql://localhost:3306/gcash";
+        String userName = "root";
+        String password = "123";
+
+        try
+        {
+            connection = DriverManager.getConnection(dburl, userName, password);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public ResultSet execQuery(String query) {
+        ResultSet result;
+        try {
+            stmt = getDBConnection().createStatement();
+            result = stmt.executeQuery(query);
+        }
+        catch (SQLException ex) {
+            System.out.println("Exception at execQuery:dataHandler" + ex.getLocalizedMessage());
+            return null;
+        }
+        finally {
+        }
+        return result;
+    }
+
+    public int execUpdateQuery(String query) {
+        int affectedRows = 0;
+        try {
+            stmt = getDBConnection().createStatement();
+            affectedRows = stmt.executeUpdate(query);
+        } catch (SQLException ex) {
+            System.out.println("Not working");
+        }
+        return affectedRows;
+    }
 }
