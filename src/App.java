7ba9a81bf66800255ad57f96c0f20d293e2043a4
Rae <r.paulos.dev@gmail.com>
2025-04-16 21:47:55 +0800

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/DatabaseHandler.java
@@ -0,0 +1,6 @@
+import java.sql.*;
+import javax.naming.spi.DirStateFactory;
+
+public class DatabaseHandler {
+    
+}
