1413be303e9e365c97e6214a8c95e2777aef9edd
Rae <r.paulos.dev@gmail.com>
2025-05-13 20:06:05 +0800

    StartUp page

---
+++ src/Customer/StartUp/StartUpController.java
@@ -0,0 +1,48 @@
+package Customer.StartUp;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class StartUpController {
+    @FXML
+    private Button loginWithFacebookButton;
+
+    @FXML
+    private Button loginWithGoogleButton;
+
+    @FXML
+    private Button signUpWithEmailButton;
+
+    private Stage stage;
+    private Scene scene; 
+    private Parent root;
+
+    @FXML
+    public void toSignUpPageHandler(ActionEvent event) throws IOException{
+        // FXMLLoader loader = new FXMLLoader(getClass().getResource("SignUp.fxml"));
+
+        // root = loader.load();
+
+        // stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        // scene = new Scene(root);
+        // stage.setScene(scene);
+        // stage.show();
+
+    }
+}
