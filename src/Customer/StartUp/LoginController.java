c6ed6ff96f4ac27fe812d3316986c3f3b90a7cc6
Rae <r.paulos.dev@gmail.com>
2025-05-13 22:07:41 +0800

    Added elements to login

--- src/Customer/StartUp/LoginController.java
+++ src/Customer/StartUp/LoginController.java
@@ -1,5 +1,22 @@
 package Customer.StartUp;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
-    
+
+    @FXML
+    private Button btn_create;
+
+    @FXML
+    private Button btn_submit;
+
+    @FXML
+    private PasswordField pf_password;
+
+    @FXML
+    private TextField tf_email;
+
 }
