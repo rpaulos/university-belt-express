a236e933211f699b748789a53f8d77e77cd3da0e
Rae <r.paulos.dev@gmail.com>
2025-05-15 14:14:58 +0800

    get the text in sign up

--- src/Customer/StartUp/SignUpController.java
+++ src/Customer/StartUp/SignUpController.java
@@ -9,6 +9,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SignUpController {
@@ -21,8 +24,38 @@ public class SignUpController {
     private Button btn_close;
 
     @FXML
+    private Button btn_submit;
+
+    @FXML
+    private ComboBox<String> cb_partnerSchools;
+
+    @FXML
+    private PasswordField pf_password;
+
+    @FXML
+    private TextField tf_email;
+
+    @FXML
+    private TextField tf_firstName;
+
+    @FXML
+    private TextField tf_lastName;
+
+    @FXML
+    private TextField tf_phoneNumber;
+
+    @FXML
+    public void initialize() {
+        cb_partnerSchools.getItems().addAll(
+            "National University Manila",
+            "Far Easter University",
+            "University of Santo Tomas",
+            "Centro Escobar University"
+        );
+}
+    @FXML
     void toStartUpPageHandler(ActionEvent event) throws IOException{
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("StartUp.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("StartUp.fxml"));
 
         root = loader.load();
 
@@ -32,5 +65,47 @@ public class SignUpController {
         stage.show();
 
     }
+
+    @FXML
+    void submitButtonHandler(ActionEvent event) throws IOException{
+
+        String email = tf_email.getText();
+        String password = pf_password.getText();
+        String firstName = tf_firstName.getText();
+        String lastName = tf_lastName.getText();
+        String phoneNumber = tf_phoneNumber.getText();
+        String selectedSchool = cb_partnerSchools.getValue();
+
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+        System.out.println("FName: " + firstName);
+        System.out.println("LName: " + lastName);
+        System.out.println("Number: " + phoneNumber);
+        System.out.println("School: " + selectedSchool);
+
+        // Validate fields
+            // Check if email is in email format (gmail, yahoo, email, outllook, edu)
+            // Check if email and number is in use
+            // Create the account and insert to database
+            // Go back to startup
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("StartUp.fxml"));
+
+        root = loader.load();
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+
+    }
+
+    // Submit button
+        // Get text from all fields
+        // Pass text to validate function
+        // Check if email and password is already in use
+        // Create the account
+        // Go back to startup
 
 }
