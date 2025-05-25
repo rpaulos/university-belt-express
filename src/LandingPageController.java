54de648bd850f5e86cb35f16290aaae5af4945f6
Rae <r.paulos.dev@gmail.com>
2025-05-25 22:21:22 +0800

    Landing page

---
+++ src/LandingPageController.java
@@ -0,0 +1,46 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import java.io.IOException;
+
+public class LandingPageController {
+
+    @FXML
+    private Button btn_ubExpress;
+
+    @FXML
+    private Button btn_ubExpressB;
+
+    private void switchScene(ActionEvent event, String fxmlFile) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleCustomerButton(ActionEvent event) throws IOException {
+        // Parent root = FXMLLoader.load(getClass().getResource("/Customer/StartUp/StartUp.fxml"));
+        // Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        // stage.setScene(new Scene(root, 340, 740));
+        // stage.show();
+        switchScene(event, "/Customer/StartUp/StartUp.fxml");
+    }
+
+    @FXML
+    private void handleBusinessButton(ActionEvent event) throws IOException {
+        // Parent root = FXMLLoader.load(getClass().getResource("/Business/BusinessStartUp/BusinessLogin.fxml"));
+        // Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        // stage.setScene(new Scene(root, 340, 740));
+        // stage.show();
+        switchScene(event, "/Business/BusinessStartUp/BusinessLogin.fxml");
+    }
+}
